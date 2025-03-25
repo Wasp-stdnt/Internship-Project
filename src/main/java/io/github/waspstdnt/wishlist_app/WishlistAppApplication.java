56d5ef9ef6292fbd3f8047b5c4ee79d04926187f
Wasp-stdnt <st5353320@stud.duikt.edu.ua>
2025-03-25 18:52:56 +0200

    Add initial project structure

---
+++ src/main/java/io/github/waspstdnt/wishlist_app/WishlistAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.waspstdnt.wishlist_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishlistAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/waspstdnt/wishlist_app/WishlistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.waspstdnt.wishlist_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
