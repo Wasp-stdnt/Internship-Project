d868254e2b78de2a78f1a04474752e30baf6b082
Wasp-stdnt <st5353320@stud.duikt.edu.ua>
2025-03-26 12:15:42 +0200

    Add Template Entity and Repository

---
+++ src/main/java/io/github/waspstdnt/wishlist_app/models/Template.java
@@ -0,0 +1,27 @@
+package io.github.waspstdnt.wishlist_app.models;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "templates")
+public class Template {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb", nullable = false)
+    private Map<String, Object> templateFields;
+}
---
+++ src/main/java/io/github/waspstdnt/wishlist_app/repositories/TemplateRepository.java
@@ -0,0 +1,9 @@
+package io.github.waspstdnt.wishlist_app.repositories;
+
+import io.github.waspstdnt.wishlist_app.models.Template;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface TemplateRepository extends JpaRepository<Template, Long> {
+    Optional<Template> findByTitle(String name);
+}
