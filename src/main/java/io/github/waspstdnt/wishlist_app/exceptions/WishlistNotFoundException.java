9fefc3f034f03aef8dbde5452a7da10d539b79fb
Wasp-stdnt <st5353320@stud.duikt.edu.ua>
2025-03-27 12:32:48 +0200

    Add custom exception for Wishlist service

---
+++ src/main/java/io/github/waspstdnt/wishlist_app/exceptions/WishlistNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.waspstdnt.wishlist_app.exceptions;
+
+public class WishlistNotFoundException extends Exception {
+    public WishlistNotFoundException(String message) {
+        super(message);
+    }
+}
