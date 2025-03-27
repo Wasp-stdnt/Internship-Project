e14d9f6cfd106e02ca8f824fdfda026544332645
Wasp-stdnt <st5353320@stud.duikt.edu.ua>
2025-03-27 13:36:38 +0200

    Add custom exceptions for EntryService methods

---
+++ src/main/java/io/github/waspstdnt/wishlist_app/exceptions/InvalidEntryDataException.java
@@ -0,0 +1,7 @@
+package io.github.waspstdnt.wishlist_app.exceptions;
+
+public class InvalidEntryDataException extends Exception {
+    public InvalidEntryDataException(String message) {
+        super(message);
+    }
+}
